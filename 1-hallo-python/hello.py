c4297c7acf991619423c6ec72ad901f62b18e5ca
Richievdheij <99058860@mydavinci.nl>
2023-12-13 23:30:27 +0100

    Hallo-Python

---
+++ 1-hallo-python/hello.py
@@ -0,0 +1,12 @@
+# hallo-python
+print("hello wereld van")
+
+print("    pppp    y     y   ttttttt   h   h    oooo   nnnn")
+print("    p   p    y   y       t      h   h   o    o  n   n")
+print("    p   p     y y        t      h   h   o    o  n   n")
+print("    pppp       y         t      hhhhh   o    o  n   n")
+print("    p          y         t      h   h   o    o  n   n")
+print("    p          y         t      h   h   o    o  n   n")
+print("    p          y         t      h   h    oooo   n   n")
+
+# Richie van der heij 99058860
