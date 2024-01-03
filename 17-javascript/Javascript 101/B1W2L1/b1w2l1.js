e462162f0724da8fbff4c1b4ae639a6b7861a912
Richievdheij <99058860@mydavinci.nl>
2024-01-03 16:57:08 +0100

    B1W2L1

---
+++ 17-javascript/Javascript 101/B1W2L1/b1w2l1.js
@@ -0,0 +1,14 @@
+// begin hier je JavaScript commandos
+// Write your first, last name and education
+alert('Hallo, U dient 3 vragen te beantwoorden'); // Alert pop
+firstname = prompt('Voer je voornaam in');
+lastname = prompt('Voer je achternaam in');
+age = prompt('Voer je leeftijd in');
+document.write( // Displays the answers on your screen
+    `First name: ${firstname}`,  // Displays first name
+    '<br />',
+    `Last name: ${lastname}`,   // Displays last name
+    '<br />',
+    `Age: ${age}`,        // Displays age
+    '<br />',
+)
